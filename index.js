036c0577d8c4b60bcd5272e078478e9a915918ec
Pradip Sarkar <sarkar.pradip2002@gmailcom>
2021-08-10 20:38:13 +0530

    Final Commit

--- index.js
+++ index.js
@@ -9,19 +9,43 @@ function setalarm(){
     let gettime=document.querySelector('.settime').value;
     let settime=new Date(gettime);
     let timediff=settime-date;
-    if(timediff>=0)
+    console.log(timediff);
+    if(timediff>654623625)
     {
+        let redmarking=document.getElementById('redmarking');
+        let replace=document.querySelector('.settime');
+        replace.classList.add('is-invalid');
+        redmarking.classList.add('invalid-feedback');
+        redmarking.innerText="Please set the alarm within seven days from now";
+    }
+    else if(timediff>=0)
+    {
+        let redmarking=document.getElementById('redmarking');
+        let replace=document.querySelector('.settime');
+        replace.classList.remove('is-invalid');
+        redmarking.classList.remove('invalid-feedback');
+        replace.classList.add('is-valid');
+        redmarking.classList.add('valid-feedback');
+        redmarking.innerText="Your alarm is set correctly";
         setTimeout(()=>{
-            console.log("bell ringing");
             ringbell();
         },timediff);
     }
-    if(timediff<0 || gettime=="")
+    else if(timediff<0)
     {
        let redmarking=document.getElementById('redmarking');
        let replace=document.querySelector('.settime');
        replace.classList.add('is-invalid');
        redmarking.classList.add('invalid-feedback');
        redmarking.innerText="Please put a future date to set alarm";
+  }
+  else if(gettime=" ")
+  {
+    let redmarking=document.getElementById('redmarking');
+       let replace=document.querySelector('.settime');
+       replace.classList.add('is-invalid');
+       redmarking.classList.add('invalid-feedback');
+       redmarking.innerText="Please put a time to set the alarm"; 
+       console.log("Inside null time")  ;
   }
 }
